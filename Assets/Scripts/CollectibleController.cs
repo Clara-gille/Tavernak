882ec441e8a76257053f4df0bd159546c28da4e8
TomRuisseau <tom.ruisseau.300302@gmail.com>
2023-10-09 18:20:14 +0200

    tweak sword anim + collectables random

--- Assets/Scripts/CollectibleController.cs
+++ Assets/Scripts/CollectibleController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class CollectibleController : MonoBehaviour
 {
+    [Header("Spawn")] [SerializeField] 
+    private float spawnRate = 0.75f;
+    
     // Start is called before the first frame update
     void Start()
     {
         
+        if ( Random.Range(0f, 1f) > spawnRate)
+        {   
+            gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
