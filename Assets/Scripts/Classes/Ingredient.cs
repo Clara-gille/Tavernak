dddaae34ac6f077e4701b49b1661b066f821eb69
Mathis <mathis.langrand@gmail.com>
2023-10-18 09:49:07 +0200

    hana putain

--- Assets/Scripts/Classes/Ingredient.cs
+++ Assets/Scripts/Classes/Ingredient.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Ingredient")] //create a new item in Unity
 public class Ingredients : ScriptableObject
 {
-    [Serializable] public class Ingredient
+    public class Ingredient
     {
-        public string Name { get; private set; } // "CERISE"
-        public Sprite Sprite { get; private set; } // "cerise.png" for sprite rendering in inventory
+        [SerializeField] public string Name { get; private set; } // "CERISE"
+        [SerializeField] public Sprite Sprite { get; private set; } // "cerise.png" for sprite rendering in inventory
         public List<Taste> Stats= new List<Taste>(); // [["sweet", 1], ["sour", 2]]
 
         // bitter, acidic, salty, sour, spicy, sweet, creamy, crunchy should be enough ?
