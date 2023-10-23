1022520b05d8cc434ecb4e241cf33065cad47e46
Clara-gille <clara.gille@student.junia.com>
2023-10-23 17:38:33 +0200

    La marmite accepte les ingrédients

--- Assets/Scripts/Cooking/CookingSlot.cs
+++ Assets/Scripts/Cooking/CookingSlot.cs
@@ -5,9 +5,45 @@ using UnityEngine.EventSystems;
 
 public class CookingSlot : MonoBehaviour, IDropHandler
 {
-    public void OnDrop(PointerEventData eventData)                  //if item dropped on the slot
+
+    //list of items that can be cooked
+    private List<Ingredient> itemsToCook = new List<Ingredient>();
+
+    private int listMax = 3;
+
+
+    public void OnDrop(PointerEventData eventData)
     {
-        InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();              //if not occupied
-        Debug.Log(inventoryItem.name);
+        if (itemsToCook.Count < listMax){
+            InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();
+
+            AddItem(inventoryItem.ingredient);
+
+            if(inventoryItem.count != 1){
+                inventoryItem.count--;
+                inventoryItem.countText.text = inventoryItem.count.ToString();
+            }
+            else{
+                Destroy(inventoryItem.gameObject);
+            }  
+        }
+        else{
+            Debug.Log("List is full");
+        }
+       
     }
+
+    //put an item in the list
+    public void AddItem(Ingredient item)
+    {
+        itemsToCook.Add(item);
+    }
+
+
+    //remove all the items from the list
+    public void RemoveAllItems()
+    {
+        itemsToCook.Clear();
+    }
+
 }
