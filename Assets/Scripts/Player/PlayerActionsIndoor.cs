8c13f67e6590475201284eb9bbe030c290111567
Clara-gille <clara.gille@student.junia.com>
2023-10-23 15:30:08 +0200

    ouverture de l'inventaire avec F

--- Assets/Scripts/Player/PlayerActionsIndoor.cs
+++ Assets/Scripts/Player/PlayerActionsIndoor.cs
@@ -4,19 +4,26 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
 
 public class PlayerControllerIndoor : MonoBehaviour
 {
+
+    [SerializeField] public CanvasGroup InventoryCanvas;
+    [SerializeField] private bool inventoryOpened = false;
+
 
     [Header("Camera")]
     public GameObject cam;
    
     [Header("In cooking pot")] 
     [SerializeField] private float cookingPotDistance = 3f;
-    
+
     [Header ("Inputs")]
     [SerializeField] PlayerInput playerInput;
-    private bool isInCookingPot;
+    private bool isInCookingPot = false;
+
     
     
     [SerializeField] private GameObject inventoryManagerObj;
@@ -27,12 +34,30 @@ public class PlayerControllerIndoor : MonoBehaviour
     void Start()
     {
         inventoryManager = inventoryManagerObj.GetComponent<InventoryManager>();
-
     }
 
    void Update()
     {
         GetActionsInputs();
+
+
+        if (isInCookingPot && CookingPotRay)
+        { // Opening the inventory thank to the key in inputmanager
+            inventoryOpened = !inventoryOpened;
+        }
+
+        if (!inventoryOpened)
+        {
+            InventoryCanvas.alpha = 0.0f; //hide the inventory
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            InventoryCanvas.alpha = 1f;         //show the inventory
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
     }
 
     void FixedUpdate()
@@ -45,7 +70,7 @@ public class PlayerControllerIndoor : MonoBehaviour
         isInCookingPot = playerInput.actions["Cooking"].ReadValue<float>() > 0;
     }
     
-    private void CookingPotRay()
+    private bool CookingPotRay()
     {
         //ray from center of the screen
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
@@ -53,11 +78,20 @@ public class PlayerControllerIndoor : MonoBehaviour
         //cast the ray
         if (Physics.Raycast(ray, out var hit, cookingPotDistance))
         {
-           //open the inventory
+            GameObject other = hit.collider.gameObject;
+            if (other.CompareTag("CookingPot"))
+            {
+                return true;   
+            }
 
+            else return false;
         }
-
+        else return false;
+        
+       
     }
+
+
 
 
 }
