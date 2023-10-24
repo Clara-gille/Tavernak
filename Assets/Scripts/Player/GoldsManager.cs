8e89508836d44beb9caa8b56ffa374de0cb089a6
TomRuisseau <tom.ruisseau.300302@gmail.com>
2023-10-24 21:11:35 +0200

    Musique outside

--- Assets/Scripts/Player/GoldsManager.cs
+++ Assets/Scripts/Player/GoldsManager.cs
@@ -8,7 +8,7 @@ using UnityEngine.SceneManagement;
 
 public class GoldsManager : MonoBehaviour
 {
-    private static int _golds = 11;
+    private static int _golds = 0;
     [SerializeField] private TextMeshProUGUI goldsText;
     
     private void Start()
